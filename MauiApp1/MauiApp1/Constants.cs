52bf0c32a1a9dbd0cf894520a21cab6a136c9044
Arseny20032 <94858362+Arseny20032@users.noreply.github.com>
2023-05-27 18:12:37 +0300

    first commit

---
+++ MauiApp1/MauiApp1/Constants.cs
@@ -0,0 +1,15 @@
+﻿//using Org.XmlPull.V1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+{
+    public static class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapAPI = "4fedbb6b5552ccabd12e2456f9084311";
+    }
+}
